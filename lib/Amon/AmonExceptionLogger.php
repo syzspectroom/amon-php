a5e8fc1c267ec4fcce8cd4b315b4b73048db0b46
blacky <syzspectroom@gmail.com>
2012-02-23 21:41:16 +0000

    exception logger started

---
+++ lib/Amon/AmonExceptionLogger.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: blacky
+ * Date: 23.02.12
+ * Time: 20:58
+ * To change this template use File | Settings | File Templates.
+ */
+class AmonExceptionLogger
+{
+
+}
---
+++ lib/Amon/Data/ExceptionData.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: blacky
+ * Date: 23.02.12
+ * Time: 21:05
+ * To change this template use File | Settings | File Templates.
+ */
+class ExceptionData
+{
+
+}
